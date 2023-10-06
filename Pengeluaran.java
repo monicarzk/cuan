673471781b32eb23643345d44004a5b0a06c58d4
monicarzk <monikarizki1933@gmail.com>
2023-10-07 00:59:37 +0700

    menambahkan data pengeluaran

--- Pengeluaran.java
+++ Pengeluaran.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Pengeluaran {
+    public static void main(String[] args) {
+        ArrayList<Integer> pengeluaran = new ArrayList<>();
+
+        pengeluaran.add(100);
+        pengeluaran.add(50);
+        pengeluaran.add(200);
+
+        System.out.println("Data Pengeluaran:");
+        for (int item : pengeluaran){
+            System.out.println(item);
+        }
+
+    }
 }

18cd9da1858add46e2db794e1583516d36626efa
monicarzk <monikarizki1933@gmail.com>
2023-10-07 01:32:04 +0700

    menambahkan detail pengeluaran

--- Pengeluaran.java
+++ Pengeluaran.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
+import java.lang.String;
 
 public class Pengeluaran {
     public static void main(String[] args) {
         ArrayList<Integer> pengeluaran = new ArrayList<>();
 
-        pengeluaran.add(100);
-        pengeluaran.add(50);
-        pengeluaran.add(200);
+        pengeluaran.add(5000);
+        pengeluaran.add(35000);
+        pengeluaran.add(27000);
 
-        System.out.println("Data Pengeluaran:");
-        for (int item : pengeluaran){
-            System.out.println(item);
-        }
+        System.out.println("Data Pengeluaran :");
+
+        String pengeluaran1 = String.valueOf(pengeluaran.get(0));;
+        String pengeluaran2 = String.valueOf(pengeluaran.get(1));
+        String pengeluaran3 = String.valueOf(pengeluaran.get(2));;
+
+        System.out.println(pengeluaran1 + " beli seblak");
+        System.out.println(pengeluaran2 + " renovasi kamar");
+        System.out.println(pengeluaran3 + " laundry pakaian");
+
 
     }
 }
